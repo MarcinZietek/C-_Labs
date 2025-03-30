5e435ed7d6772453c931350f218c38d45ad7d9a9
unknown <marcin.it.zietek@gmail.com>
2025-03-30 11:27:22 +0200

    pierwszy program - rozgrzewka

---
+++ C-_Labs/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Zadanie 1
+Console.WriteLine("Hello, World!");
+
+// Zadanie 2
+
+Console.WriteLine("Podaj swoje imię: ");
+var name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+
+// Zadanie 3
+
+//int result = 5 + 9
+
