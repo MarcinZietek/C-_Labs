181aa1212be71482e8a660e81e0c8986119bce92
unknown <marcin.it.zietek@gmail.com>
2025-03-30 12:47:17 +0200

    Instrukcje sterujące i pętle - Zad1 Zad2 Zad3

--- C-_Labs/Program.cs
+++ C-_Labs/Program.cs
@@ -2,14 +2,49 @@
 
 
 
-/* OPERATORY */
+/* INSTRUKCJE STERUJĄCE I PĘTLE */
 
-// Zadanie 5
+// Zadanie 1
+Console.WriteLine("Zadanie 1");
+int n1 = 10, n2 = 20;
 
-string a = "Ala ,";
-string b = " ma ,";
-string c = "kota.";
-string result = a + b + c;
-Console.WriteLine(result);
+if (n1 <= n2)
+{
+    Console.WriteLine("n2 jest większe od n1 lub n1 jest równe n2");
+}
+else
+{
+    Console.WriteLine("Warunki zadania nie zostały spełnione");
+}
+Console.WriteLine("----------------------------------------------------------");
 
-/* Zmienne o typie string zostały uwzględniają znaki białe podczas wyświetlania na konsoli */
+// Zadanie 2
+Console.WriteLine("Zadanie 2");
+string text = "C#";
+int j = 1;
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine(i + ": " + text);
+}
+Console.WriteLine("----------------------------------------------------------");
+while (j < 11) {  Console.WriteLine($"{j}: {text}");
+j++;
+};
+
+// Zadanie 3
+
+int n = 10;
+for (int i = 0; i < n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i} - Parzysta");
+    }
+    else
+    {
+        Console.WriteLine($"{i} - Nieparzysta");
+    }
+}
+
+
