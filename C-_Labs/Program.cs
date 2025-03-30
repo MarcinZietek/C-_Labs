ca69cc3c0ab2ff6e2eecd0a15f2e482bc630a251
unknown <marcin.it.zietek@gmail.com>
2025-03-30 11:42:51 +0200

    Operatory - Zad1

--- C-_Labs/Program.cs
+++ C-_Labs/Program.cs
@@ -1,15 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+/* OPERATORY */
+
 // Zadanie 1
-Console.WriteLine("Hello, World!");
 
-// Zadanie 2
+int number = 0;
+float money = 0;
+string text = string.Empty;
+bool isLogged = false;
+char myChar = 'a';
+decimal price = 0;
 
-Console.WriteLine("Podaj swoje imię: ");
-var name = Console.ReadLine();
-Console.WriteLine("Hello " + name);
-
-// Zadanie 3
-
-//int result = 5 + 9
+Console.WriteLine($"Zmienne: int: {number}, float: {money}, string: {text}, bool: {isLogged}, char: {myChar}, decimal: {price}");
 
