dbef03329ac82a7c9d2ecbcf2ca6889a5cb41adb
unknown <marcin.it.zietek@gmail.com>
2025-03-30 12:16:34 +0200

    Operatory - Zad5

--- C-_Labs/Program.cs
+++ C-_Labs/Program.cs
@@ -4,36 +4,12 @@
 
 /* OPERATORY */
 
+// Zadanie 5
 
-// Zadanie 2
-
-string myAge = "Age: ";
-int wifeAge = 18;
-string result = myAge + wifeAge;
+string a = "Ala ,";
+string b = " ma ,";
+string c = "kota.";
+string result = a + b + c;
 Console.WriteLine(result);
 
-/* Żeby łączyć zmienne, w tym przypadku string z integer, można użyć operatora +. */
-
-// Zadanie 3
-
-bool isTrue = true;
-bool isFalse = false;
-bool isReallyTrue = true;
-
-bool and = isTrue && isFalse;
-bool or = isTrue || isFalse;
-bool negative = !isFalse;
-
-Console.WriteLine($"Zmienne logiczne: {and}, {or}, {negative}");
-
-// Zadanie 4
-
-int a = 5;
-int b = 12;
-int add = a + b;
-int sub = a - b;
-int mul = a * b;
-int div = a / b;
-int mod = a % b;
-Console.WriteLine($"Wyniki działań matematycznych: dodawanie - {add}, odejmowanie - {sub}, mnożenie - {mul}, dzielenie - {div}, modulo - {mod}");
-
+/* Zmienne o typie string zostały uwzględniają znaki białe podczas wyświetlania na konsoli */
